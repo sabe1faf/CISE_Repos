f4e737acdc54a8fd22f1532e2dfbd21cbd363827
sabe1faf <3286174498@qq.com>
2025-09-12 09:48:43 +0800

    fix(react-app): update text in App.js to agile principles

--- worksheet1/cise-react-learn/src/App.js
+++ worksheet1/cise-react-learn/src/App.js
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Agile is about Values and Principles
+        </a>  
       </header>
     </div>
   );
